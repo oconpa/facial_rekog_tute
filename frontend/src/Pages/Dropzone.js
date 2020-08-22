a70329aa6d39442dfbceaaa2cfe6d0625313b764
EC2 Default User <ec2-user@ip-172-31-21-96.ap-southeast-2.compute.internal>
2020-08-22 03:08:29 +0000

    update

--- frontend/src/Pages/Dropzone.js
+++ frontend/src/Pages/Dropzone.js
@@ -2,11 +2,27 @@ import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
 import axios from "axios";
 import ImageUploader from 'react-images-upload';
+import SimpleAccordion from '../Components/Acordion'
+import { useToasts } from 'react-toast-notifications'
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > * + *': {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
 
 function MyDropzone() {
+  const classes = useStyles();
   const [pictures, setPictures] = useState([])
   const [image, setImage] = useState(false)
   const [scan, setScan] = useState([])
+  const { addToast } = useToasts()
+  const [loading, setLoading] = useState(false)
 
   const onDrop = (event) => {
     console.log(event[0])
@@ -18,6 +34,7 @@ function MyDropzone() {
   }
 
   const uploadFile = () => {
+    setLoading(true)
     axios(
       "https://8qohygpr1k.execute-api.ap-southeast-2.amazonaws.com/dev/upload?fileName=" +
             pictures[0].name
@@ -27,7 +44,7 @@ function MyDropzone() {
         const url = response.data;
 
         // Initiating the PUT request to upload file  
-        console.log(pictures[0])  
+        console.log(pictures[0])
         axios({
             method: "PUT",
             url: url,
@@ -44,12 +61,11 @@ function MyDropzone() {
                 console.log(res.data.FaceDetails)
                 setImage(false)
                 setScan(res.data.FaceDetails)
+                addToast('Saved To Gallery', { appearance: 'success' })
+                setLoading(false)
               })
               .catch(err => {
-                // this.setState({
-                //     error: "Error Occured while uploading the file",
-                //     uploadSuccess: undefined
-                // });
+                addToast(err, { appearance: 'error' })
                 console.log(err)
               });
             })
@@ -64,38 +80,33 @@ function MyDropzone() {
 }
 
   return (
-    <div style={{width: 400, margin: 'auto'}}>
-      <ImageUploader
-        withPreview={true}
-        withIcon={true}
-        buttonText='Choose images'
-        onChange={onDrop}
-        imgExtension={['.jpg', '.gif', '.png']}
-        maxFileSize={5242880}
-        singleImage={true}
-      />
-      { image ?
-      <div style={{margin: 'auto', width: 100}}>
-        <Button variant="contained" color="primary" onClick={uploadFile}>
-          Scan
-        </Button>
-      </div>
-      : <div>{scan.map(x => {
-          console.log(x)
-          return(
-          <div>
-            <h2>Box Detection</h2>
-            {x.Emotions.map(y => {
-              console.log(y)
-              return(
-                <li>{y.Type.toString() + ', ' + y.Confidence.toString()}</li>
-                )    
-            })}
-          </div>
-          )
-      })}</div>
+    <div>
+      { loading ? 
+        <div className={classes.root}>
+          <CircularProgress style={{margin: 'auto'}} />
+        </div>
+        :
+        <SimpleAccordion data={scan}/>
       }
-
+      <div style={{width: 400, margin: 'auto'}}>
+        <ImageUploader
+          withPreview={true}
+          withIcon={true}
+          buttonText='Choose images'
+          onChange={onDrop}
+          imgExtension={['.jpg', '.gif', '.png']}
+          maxFileSize={5242880}
+          singleImage={true}
+        />
+        { image ?
+        <div style={{margin: 'auto', width: 100}}>
+          <Button variant="contained" color="primary" onClick={uploadFile}>
+            Scan
+          </Button>
+        </div>
+        : null
+        }
+      </div>
     </div>
   )
 }
