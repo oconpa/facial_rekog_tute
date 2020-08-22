1c9c0b8b455f3bc51be8c33b28c7aeaf6bb57909
Patrick <oconpa@gmail.com>
2020-08-22 03:11:11 +0000

    update

--- backend/lambda.py
+++ backend/lambda.py
@@ -1,23 +1,61 @@
 import boto3
+import json
+from botocore.exceptions import ClientError
+
 
 client = boto3.client('rekognition')
+s3 = boto3.client('s3')
+bucket_name = "facial-detection-pat"
+expiration = 120
+object_name = "test.jpg"
 
-# def handler(event, context):
-#     message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
-    
-#     return { 
-#         'message' : message
-#     }
 
-response = client.detect_faces(
-    Image={
-        'S3Object': {
-            'Bucket': 'facial-detection-pat',
-            'Name': 'download.jpg',
+def handler(event, context):
+    print(event)
+    if (event['path'] == '/upload'):
+        try:
+            response = s3.generate_presigned_url(
+                        'put_object',
+                        Params={
+                            'Bucket': bucket_name,
+                            'Key': event['queryStringParameters']['fileName'],
+                            'ContentType': 'multipart/form-data'
+                        },
+                        ExpiresIn=expiration)
+        except ClientError as e:
+            logging.error(e)
+            return None
+
+        return {
+            'statusCode': 200,
+            'headers': {
+                "access-control-allow-origin": "*"
+            },
+            'body': json.dumps(response)
         }
-    },
-    Attributes=[
-        'ALL',
-    ]
-)
-print(response)
+    elif (event['path'] == '/detect'):
+        response = client.detect_faces(
+            Image={
+                'S3Object': {
+                    'Bucket': bucket_name,
+                    'Name': event['body'],
+                }
+            },
+            Attributes=['ALL']
+        )
+        
+        s3.put_object(
+            Body=(bytes(json.dumps(response).encode('UTF-8'))),
+            Bucket=bucket_name,
+            Key=str(event['body'][:-4]) + '.json',
+        )
+
+        
+        print(response)
+        return {
+            'statusCode': 200,
+            'headers': {
+                "access-control-allow-origin": "*"
+            },
+            'body': json.dumps(response)
+        }
