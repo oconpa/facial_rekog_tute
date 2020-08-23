cda656edda5177d29680f8bb69c1e630f59aed02
Patrick <oconpa@gmail.com>
2020-08-23 03:30:09 +0000

    update

--- frontend/src/Pages/Gallery.js
+++ frontend/src/Pages/Gallery.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import ImageGridList from './ImageGridList'
+import Charts from './Charts'
 
 function Gallery() {
     return (
-      <div style={{padding: 24}}>
+      <div style={{padding: 24, display: 'inline-block', width: '95%'}}>
+        <div>
         <Typography variant="h4" gutterBottom>
           Gallery
         </Typography>
-        <p>Select an image by select the top left tick to analyse it's facial detection response</p>
+          <p>Select an image by select the top left tick to analyse it's facial detection response. On the bottom of this page are also some charts performing data anaysis on the images you upload.
+          </p>
         <ImageGridList />
+        </div>
+        <div>
+          <Charts />
+        </div>
       </div>
     )
 }
