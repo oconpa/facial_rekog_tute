9546bb2167bb4315e6ae55460f992e565d013b2f
oconpa <35761519+oconpa@users.noreply.github.com>
2020-09-02 14:45:23 +1000

    update

--- frontend/src/Components/ImageGridList.js
+++ frontend/src/Components/ImageGridList.js
@@ -57,7 +57,7 @@ function ImageGridList() {
         setDrawer(true)
         
         const apiURL = 'REPLACE ME'
-        const detectURL = `${apiURL}/detect`
+        const detectURL = `${REPLACE ME}/detect`
         const imageId = IMAGES[event].src.split('/')[3].split('?')[0]
         
         const res = await fetch(detectURL, { method: 'POST', body: imageId })
